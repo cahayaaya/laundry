093d8dca39203bd8f8e7f5f0e804db23e1f2a7e0
cahayaaya <cahayanss14@gmail.com>
2025-02-05 13:34:56 +0700

    menambah kotlin pada activity tambah pelanggan agar bisa ber pindah halaman

--- app/src/main/java/com/cahayaaya/laundry/MainActivity.kt
+++ app/src/main/java/com/cahayaaya/laundry/MainActivity.kt
@@ -1,16 +1,39 @@
 package com.cahayaaya.laundry
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.cahayaaya.laundry.pegawai.DataPegawai
+import com.cahayaaya.laundry.pelanggan.DataPelanggan
+
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var pegawai : CardView
+    lateinit var pelanggan: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        pegawai=findViewById(R.id.cvlima)
+        pelanggan=findViewById(R.id.customer)
+
+        pegawai.setOnClickListener{
+            val intent = Intent(this,DataPegawai::class.java)
+            startActivity(intent)
+        }
+        pelanggan.setOnClickListener{
+            val intent = Intent(this,DataPelanggan::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/cahayaaya/laundry/pelanggan/DataPelanggan.kt
+++ app/src/main/java/com/cahayaaya/laundry/pelanggan/DataPelanggan.kt
@@ -1,21 +1,36 @@
 package com.cahayaaya.laundry.pelanggan
 
+import android.content.Intent
 import android.os.Bundle
+import android.service.controls.actions.FloatAction
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.cahayaaya.laundry.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class DataPelanggan : AppCompatActivity() {
+    lateinit var bt_data_pengguna_tambah : FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_data_pelanggan)
+        init()
+        tekan()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+    fun init() {
+        bt_data_pengguna_tambah = findViewById(R.id.bt_data_pengguna_tambah)
+    }
+    fun tekan() {
+        bt_data_pengguna_tambah.setOnClickListener {
+            val intent = Intent(this,TambahPelanggan::class.java)
+            startActivity(intent)
         }
     }
 }
